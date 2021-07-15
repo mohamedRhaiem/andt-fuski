658ad57d71056c5b4663b933661bd1564d506114
Mohamed Ben Rhaiem <mohamed.ben@g.globo>
2021-07-15 11:56:29 -0300

    Feature/add select api component

--- src/components/form/utils/option.ts
+++ src/components/form/utils/option.ts
@@ -1,8 +1,6 @@
 import { isArray } from "lodash";
 
 export const buildDisplay = (item: any, params: string | string[]) => {
-  console.log(item);
-  console.table(params);
   if (isArray(params)) {
     return item[params[0]] +'-'+ item[params[1]];
   }
